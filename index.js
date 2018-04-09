658ec155dfca3c2a05ce624c20bdd6f179a25aef
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-09 14:55:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,14 @@ function currentLine(katzDeliLine){
       newArray.push('${i+1}.${katzDeliLine[i]}');
       return "The line is currently: ${newArray.join(", ")}";}}}
   
-  
+  function currentLine(katzDeliLine) {
++  var array = []
++  if (katzDeliLine.length === 0) {
++    return "The line is currently empty."
++  } else {
++    for (var i = 0; i < katzDeliLine.length; i++) {
++      array.push(`${i + 1}. ${katzDeliLine[i]}`)
++       } 
++        return `The line is currently: ${array.join(", ")}`
++  }
++}
